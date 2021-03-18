0f92a8758d630648ee5c70070a08d6aa9dfd11c8
npwaters <40352015+npwaters@users.noreply.github.com>
2021-03-18 17:52:33 +1100

    feat: Convert Vuex Store to Typescript

--- store/todos.ts
+++ store/todos.ts
@@ -8,10 +8,10 @@ export const state = () => ({
 export type RootState = ReturnType<typeof state>
 
 export const mutations: MutationTree<RootState> = {
-  setInitial(state, todos) {
-    state.list = todos
+  setInitial(state: RootState, todos: TodoItem[]) {
+    state.list.push(...todos)
   },
-  add(state, text) {
+  add(state: RootState, text: string) {
     state.list.push({
       text,
       done: false,
@@ -20,14 +20,19 @@ export const mutations: MutationTree<RootState> = {
   remove(state, { todo }) {
     state.list.splice(state.list.indexOf(todo), 1)
   },
-  toggle(state, todo) {
+  toggle(state, todo: TodoItem) {
     todo.done = !todo.done
   },
 }
 
 export const actions: ActionTree<RootState, RootState> = {
   async retrieveInitial({ commit }) {
-    const todos = await this.$axios.$get('/api')
+    const result = await this.$axios.$get('/api')
+    const todos: TodoItem[] = []
+    for (const item of result) {
+      const todo: TodoItem = item
+      todos.push(todo)
+    }
     commit('setInitial', todos)
   },
 }
