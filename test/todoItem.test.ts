1777e9b147f5709c4fa44b9dcbeb218764ba7e78
npwaters <40352015+npwaters@users.noreply.github.com>
2021-03-22 15:44:26 +1100

    fix: Missing bootstrap references in tests

--- test/todoItem.test.ts
+++ test/todoItem.test.ts
@@ -1,9 +1,14 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import TodoItem from '@/components/todoItem.vue'
 
 describe('TodoItem', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(TodoItem)
+    const wrapper = shallowMount(TodoItem, {
+      stubs: { 'b-container': true, 'b-row': true, 'b-col': true },
+      propsData: {
+        todo: { id: '1', text: '1', done: false }
+      }
+    })
     expect(wrapper.vm).toBeTruthy()
   })
 })
