81a98dec9fb674c76851dc92c83f1a7b80c3ca53
npwaters <40352015+npwaters@users.noreply.github.com>
2021-03-22 19:43:45 +1100

    chore: Tidy up code

--- test/todos.test.ts
+++ test/todos.test.ts
@@ -43,29 +43,4 @@ describe('store/todos.ts', () => {
       expect(todos.length).toBe(3)
     })
   })
-
-  // describe('byStudio', () => {
-  //   let byStudio
-  //
-  //   beforeEach(() => {
-  //     byStudio = store.getters['movies/byStudio']
-  //   })
-  //
-  //   test('is a function', () => {
-  //     expect(_.isFunction(byStudio)).toBe(true)
-  //   })
-  //
-  //   test('matches what the disney getter returns', () => {
-  //     const movies = store.getters['movies/byStudio']('Disney')
-  //     const disney = store.getters['movies/disney']
-  //     expect(movies).toEqual(disney)
-  //   })
-  //
-  //   test('shows all other studios with one entry each', () => {
-  //     expect(byStudio('sony').length).toBe(1)
-  //     expect(byStudio('warner bros.').length).toBe(1)
-  //     expect(byStudio('universal').length).toBe(1)
-  //     expect(byStudio('beijing enlight').length).toBe(1)
-  //   })
-  // })
 })
